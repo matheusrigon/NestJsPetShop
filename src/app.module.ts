218cfa5e3a529ba270f3d8696462cf7cb13f6945
matheusrigon <matheusjacobirigon@gmail.com>
2025-03-02 11:03:29 -0300

    Local env changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,18 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { BackofficeModule } from 'src/modules/backoffice/backoffice.module';
+import { StoreModule } from 'src/modules/store/store.module';
 import configuration from 'configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: 'local.env',
+      envFilePath: '.env.local',
       load: [configuration]
     }),
     MongooseModule.forRoot(process.env.CONNECTION_STRING),
-    BackofficeModule
+    BackofficeModule,
+    StoreModule
   ],
   controllers: [],
   providers: [],
